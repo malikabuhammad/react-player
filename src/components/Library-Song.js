cd39a9af25d090b4ad9f4915f670babc9d4f481d
malikabuhammad <71324950+malikabuhammad@users.noreply.github.com>
2023-02-26 23:00:02 +0200

    Updating data and UseEffects

--- src/components/Library-Song.js
+++ src/components/Library-Song.js
@@ -27,8 +27,10 @@ const LibrarySong=({setSongs,song,songs,setCurrentSong,id,audioRef,isPlaying})=>
 };
     
     return(
-        <div onClick={songSelectHandler} className={`library-song ${song.active? 'selected':""}`}>
-            <img alt={song.name} src={song.cover}></img>
+        <div onClick={songSelectHandler} 
+        className={`library-song ${song.active? 'selected':""}`}>
+
+            <img alt={song.name} src={song.cover}/>
             <div className="song-description">
 
             <h3>{song.name}</h3>
